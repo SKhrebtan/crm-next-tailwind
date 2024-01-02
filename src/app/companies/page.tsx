308eb9d5f6b98d8dbadbbee8536ee78690364340
123 <mac@Macs-Mac-mini.local>
2024-01-02 17:39:48 +0200

    first try

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface CompaniesPage {
+
+}
+
+export default function Companies({}: CompaniesPage) {
+
+  return (
+    <main>
+    <h1 className='text-xl'>Companies Page</h1>
+    </main>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface DashboardPage {
+
+}
+
+export default function Dashboard({}: DashboardPage) {
+
+  return (
+    <main>
+    <h1 className='text-xl'>Dashboard Page</h1>
+    </main>
+  )
+}
+
